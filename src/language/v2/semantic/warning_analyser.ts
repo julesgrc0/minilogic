94f58ce9583a65e4b036f707d06f5ed743b58180
julesgrc0 <julesgrc0@gmail.com>
2025-04-26 11:15:52 +0200

    Enhance SemanticWarningAnalyser: add support for Operators, refine function parameter checks, and improve expression handling for unused variables and functions.

--- src/language/v2/semantic/warning_analyser.ts
+++ src/language/v2/semantic/warning_analyser.ts
@@ -1,4 +1,4 @@
-import { Keywords } from "../lexer";
+import { Keywords, Operators } from "../lexer";
 import {
   Expression,
   ExpressionType,
@@ -11,6 +11,7 @@ enum SemanticWarningType {
   VariableUnused,
   FunctionUnused,
   FunctionUnusedParameter,
+  FunctionToVariable,
 }
 
 type SemanticWarning = {
@@ -32,6 +33,8 @@ class SemanticWarningAnalyser {
     [SemanticWarningType.FunctionUnused]: "Function {0} is unused",
     [SemanticWarningType.FunctionUnusedParameter]:
       "Function {0} has {1} unused parameters ({2})",
+    [SemanticWarningType.FunctionToVariable]:
+      "Function {0} can be converted to a variable",
   };
 
   public constructor(private program: Statement[]) {}
@@ -115,18 +118,6 @@ class SemanticWarningAnalyser {
         stmt,
         new_object
       );
-      return;
-    }
-
-    new_object = this.simplifyNotExpression(stmt.value);
-    if (new_object) {
-      this.pushWarning(
-        SemanticWarningType.ExpressionOptimized,
-        null,
-        stmt,
-        new_object
-      );
-      return;
     }
   }
 
@@ -138,7 +129,9 @@ class SemanticWarningAnalyser {
     }
     this.updateExpressionCount(stmt.body, true);
 
-    const unused = this.findUnusedFunctionParameters(stmt);
+    const unused = this.findUnusedFunctionParameters(stmt.parameters, [
+      stmt.body,
+    ]);
     if (unused.length > 0) {
       this.pushWarning(
         SemanticWarningType.FunctionUnusedParameter,
@@ -161,14 +154,12 @@ class SemanticWarningAnalyser {
       );
     }
 
-    new_object = this.simplifyNotExpression(stmt.body);
-    if (new_object) {
-      this.pushWarning(
-        SemanticWarningType.ExpressionOptimized,
-        null,
-        stmt,
-        new_object
-      );
+    if (stmt.body.type == ExpressionType.Number) {
+      this.pushWarning(SemanticWarningType.FunctionToVariable, null, stmt, {
+        ...stmt,
+        type: StatementType.Variable,
+        value: stmt.body,
+      });
     }
   }
 
@@ -179,7 +170,10 @@ class SemanticWarningAnalyser {
       this.functions[stmt.name] = { stmt, count: 0 };
     }
 
-    const unused = this.findUnusedFunctionTableParameters(stmt);
+    const unused = this.findUnusedFunctionParameters(
+      [...stmt.parameters, ...stmt.subparameters],
+      stmt.table.map((e) => e.value)
+    );
     if (unused.length > 0) {
       this.pushWarning(
         SemanticWarningType.FunctionUnusedParameter,
@@ -196,16 +190,6 @@ class SemanticWarningAnalyser {
       this.updateExpressionCount(expr.value, true);
 
       let new_object = this.removeDeadExpression(expr.value);
-      if (new_object) {
-        this.pushWarning(
-          SemanticWarningType.ExpressionOptimized,
-          null,
-          stmt,
-          new_object
-        );
-      }
-
-      new_object = this.simplifyNotExpression(expr.value);
       if (new_object) {
         this.pushWarning(
           SemanticWarningType.ExpressionOptimized,
@@ -235,135 +219,172 @@ class SemanticWarningAnalyser {
     expr: Expression,
     needReference: boolean = false
   ) {
-      switch (expr.type) {
-        case ExpressionType.Variable:
-          if (
-            this.variables.hasOwnProperty(expr.name) &&
-            (needReference || expr.reference)
-          ) {
-            this.variables[expr.name].count++;
-          }
-          break;
-        case ExpressionType.BuiltinCall:
-        case ExpressionType.FunctionCall:
-          if (
-            expr.type == ExpressionType.FunctionCall &&
-            this.functions.hasOwnProperty(expr.name)
-          ) {
-            this.functions[expr.name].count++;
-          }
-          expr.parameters.forEach((param) => this.updateExpressionCount(param, true));
-          break;
-        case ExpressionType.Binary:
-          this.updateExpressionCount(expr.left, needReference);
-          this.updateExpressionCount(expr.right, needReference);
-          break;
-        case ExpressionType.Unary:
-          this.updateExpressionCount(expr.operand, needReference);
-          break;
-      }
+    switch (expr.type) {
+      case ExpressionType.Variable:
+        if (
+          this.variables.hasOwnProperty(expr.name) &&
+          (needReference || expr.reference)
+        ) {
+          this.variables[expr.name].count++;
+        }
+        break;
+      case ExpressionType.BuiltinCall:
+      case ExpressionType.FunctionCall:
+        if (
+          expr.type == ExpressionType.FunctionCall &&
+          this.functions.hasOwnProperty(expr.name)
+        ) {
+          this.functions[expr.name].count++;
+        }
+        expr.parameters.forEach((param) =>
+          this.updateExpressionCount(param, true)
+        );
+        break;
+      case ExpressionType.Binary:
+        this.updateExpressionCount(expr.left, needReference);
+        this.updateExpressionCount(expr.right, needReference);
+        break;
+      case ExpressionType.Unary:
+        this.updateExpressionCount(expr.operand, needReference);
+        break;
+    }
   }
 
   private removeDeadExpression(expr: Expression): Expression | null {
+    if (expr.type === ExpressionType.Binary) {
+      const left = this.removeDeadExpression(expr.left) ?? expr.left;
+      const right = this.removeDeadExpression(expr.right) ?? expr.right;
+
+      if (expr.operator === Operators.Or) {
+        if (left.type === ExpressionType.Number && left.value === 1)
+          return left;
+        if (right.type === ExpressionType.Number && right.value === 1)
+          return right;
+
+        if (left.type === ExpressionType.Number && left.value === 0)
+          return right;
+        if (right.type === ExpressionType.Number && right.value === 0)
+          return left;
+
+        if (JSON.stringify(left) === JSON.stringify(right)) return left;
+      }
+
+      if (expr.operator === Operators.And) {
+        if (left.type === ExpressionType.Number && left.value === 0)
+          return left;
+        if (right.type === ExpressionType.Number && right.value === 0)
+          return right;
+
+        if (left.type === ExpressionType.Number && left.value === 1)
+          return right;
+        if (right.type === ExpressionType.Number && right.value === 1)
+          return left;
+
+        if (JSON.stringify(left) === JSON.stringify(right)) return left;
+      }
+
+      return { ...expr, left, right };
+    } else if (
+      expr.type === ExpressionType.Unary &&
+      expr.operator === Operators.Not
+    ) {
+      const subExpr = this.removeDeadExpression(expr.operand) ?? expr.operand;
+
+      if (
+        subExpr.type === ExpressionType.Unary &&
+        subExpr.operator === Operators.Not
+      ) {
+        return this.removeDeadExpression(subExpr.operand);
+      } else if (subExpr.type === ExpressionType.Binary) {
+        const left = this.removeDeadExpression(subExpr.left) ?? subExpr.left;
+        const right = this.removeDeadExpression(subExpr.right) ?? subExpr.right;
+
+        const regroupeCases: Omit<
+          Record<Operators, Operators>,
+          Operators.Not
+        > = {
+          [Operators.And]: Operators.Nand,
+          [Operators.Or]: Operators.Nor,
+          [Operators.Xor]: Operators.Xnor,
+          [Operators.Imply]: Operators.Nimply,
+
+          [Operators.Nand]: Operators.And,
+          [Operators.Nor]: Operators.Or,
+          [Operators.Xnor]: Operators.Xor,
+          [Operators.Nimply]: Operators.Imply,
+        };
+
+        if (subExpr.operator in regroupeCases) {
+          return {
+            ...subExpr,
+            left,
+            operator:
+              regroupeCases[subExpr.operator as keyof typeof regroupeCases],
+            right,
+          };
+        }
+      }
+    } else if (
+      expr.type == ExpressionType.FunctionCall ||
+      expr.type == ExpressionType.BuiltinCall
+    ) {
+      let new_params = expr.parameters.map((param) =>
+        this.removeDeadExpression(param)
+      );
+
+      if (new_params.every((param) => param == null)) {
+        return null;
+      }
+
+      return {
+        ...expr,
+        parameters: new_params.map((param, index) => {
+          if (param == null) {
+            return expr.parameters[index];
+          }
+          return param;
+        }),
+      };
+    }
+
     return null;
   }
-  private simplifyNotExpression(expr: Expression): Expression | null {
-    return null;
+
+  private findUnusedFunctionParameters(
+    parameters: string[],
+    exprs: Expression[]
+  ): string[] {
+    const usedParams = new Set<string>();
+
+    const checkExpression = (expr: Expression) => {
+      if (expr.type === ExpressionType.Variable) {
+        if (!expr.reference && !usedParams.has(expr.name)) {
+          usedParams.add(expr.name);
+        }
+        return;
+      }
+
+      if (
+        expr.type === ExpressionType.FunctionCall ||
+        expr.type === ExpressionType.BuiltinCall
+      ) {
+        for (const param of expr.parameters) {
+          checkExpression(param);
+        }
+      } else if (expr.type === ExpressionType.Binary) {
+        checkExpression(expr.left);
+        checkExpression(expr.right);
+      } else if (expr.type === ExpressionType.Unary) {
+        checkExpression(expr.operand);
+      }
+    };
+
+    for (const expr of exprs) {
+      checkExpression(expr);
+    }
+
+    return parameters.filter((param) => !usedParams.has(param));
   }
-
-  private findUnusedFunctionParameters(stmt: Statement): string[] {
-    if (stmt.type !== StatementType.Function) return [];
-
-    return [];
-  }
-
-  private findUnusedFunctionTableParameters(stmt: Statement): string[] {
-    if (stmt.type !== StatementType.FunctionTable) return [];
-    return [];
-  }
-
-  //   private removeDeadCode(expr: Expression): Expression {
-  //     if (expr.type === ExpressionType.BinaryExpression) {
-  //       const left = this.removeDeadCode(expr.left);
-  //       const right = this.removeDeadCode(expr.right);
-
-  //       if (expr.operator === Operators.Or) {
-  //         if (left.type === ExpressionType.Number && left.value === 1)
-  //           return left;
-  //         if (right.type === ExpressionType.Number && right.value === 1)
-  //           return right;
-
-  //         if (left.type === ExpressionType.Number && left.value === 0)
-  //           return right;
-  //         if (right.type === ExpressionType.Number && right.value === 0)
-  //           return left;
-
-  //         if (JSON.stringify(left) === JSON.stringify(right)) return left;
-  //       }
-
-  //       if (expr.operator === Operators.And) {
-  //         if (left.type === ExpressionType.Number && left.value === 0)
-  //           return left;
-  //         if (right.type === ExpressionType.Number && right.value === 0)
-  //           return right;
-
-  //         if (left.type === ExpressionType.Number && left.value === 1)
-  //           return right;
-  //         if (right.type === ExpressionType.Number && right.value === 1)
-  //           return left;
-
-  //         if (JSON.stringify(left) === JSON.stringify(right)) return left;
-  //       }
-
-  //       return { ...expr, left, right };
-  //     }
-
-  //     return expr;
-  //   }
-
-//   private simplifyNot(expr: Expression): Expression {
-//     if (
-//       expr.type === ExpressionType.UnaryExpression &&
-//       expr.operator === Operators.Not
-//     ) {
-//       const subExpr = expr.operand;
-//       if (
-//         subExpr.type === ExpressionType.UnaryExpression &&
-//         subExpr.operator === Operators.Not
-//       ) {
-//         return this.simplifyNot(subExpr.operand);
-//       } else if (subExpr.type === ExpressionType.BinaryExpression) {
-//         const regroupeCases: Omit<
-//           Record<Operators, Operators>,
-//           Operators.Not
-//         > = {
-//           [Operators.And]: Operators.Nand,
-//           [Operators.Or]: Operators.Nor,
-//           [Operators.Xor]: Operators.Xnor,
-//           [Operators.Imply]: Operators.Nimply,
-//           [Operators.Equal]: Operators.Nequal,
-
-//           [Operators.Nand]: Operators.And,
-//           [Operators.Nor]: Operators.Or,
-//           [Operators.Xnor]: Operators.Xor,
-//           [Operators.Nimply]: Operators.Imply,
-//           [Operators.Nequal]: Operators.Equal,
-//         };
-
-//         if (subExpr.operator in regroupeCases) {
-//           return {
-//             id: subExpr.id,
-//             type: ExpressionType.BinaryExpression,
-//             left: this.simplifyNot(subExpr.left),
-//             operator:
-//               regroupeCases[subExpr.operator as keyof typeof regroupeCases],
-//             right: this.simplifyNot(subExpr.right),
-//           };
-//         }
-//       }
-//     }
-//     return expr;
-//   }
 }
 
 export { SemanticWarningAnalyser, SemanticWarning, SemanticWarningType };
